fa31366e0d67c694cd66e18f49b180f15d68d460
UserHasBeenName <sonny.pugh.125@accesstomusic.ac.uk>
2023-06-28 14:13:54 +0100

    Added floor class
    For players to walk on.

--- main.py
+++ main.py
@@ -63,3 +63,10 @@ class Player():
         if self.is_jumping != 0:
             self.pos[1] -= 1 + self.is_jumping 
             self.is_jumping -= 1
+
+class Floor():
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface((WIDTH, 15))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT))
