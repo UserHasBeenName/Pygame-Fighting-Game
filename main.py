91535b7053885a56bd838bee6b98631d8f26c52c
UserHasBeenName <sonny.pugh.125@accesstomusic.ac.uk>
2023-06-28 14:15:55 +0100

    Added physics checks

--- main.py
+++ main.py
@@ -70,3 +70,54 @@ class Floor():
         self.surf = pygame.Surface((WIDTH, 15))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT))
+
+red_player = Player((WIDTH/3, GROUND), True, (255, 100, 100), [K_a, K_SPACE, K_d])
+blue_player = Player(((WIDTH/3)*2, GROUND), False, (100, 100, 255), [K_LEFT, K_UP, K_RIGHT])
+floor = Floor()
+
+sprite_group = pygame.sprite.Group()
+
+sprite_list = [
+    floor,
+    red_player,
+    blue_player
+]
+
+for entity in sprite_list:
+    sprite_group.add(entity)
+
+
+def check_rel_position():
+    if red_player.pos[0] > blue_player.pos[0]:
+        red_player.facing_right = False
+        blue_player.facing_right = True
+    else:
+        red_player.facing_right = True
+        blue_player.facing_right = False
+
+
+def fall():
+    if red_player.pos[1] < GROUND:
+        red_player.pos[1] += 7
+    elif red_player.pos[1] > GROUND:
+        red_player.pos[1] = GROUND
+    if blue_player.pos[1] < GROUND:
+        blue_player.pos[1] += 7
+    elif blue_player.pos[1] > GROUND:
+        blue_player.pos[1] = GROUND
+
+def wall_collision():
+    if red_player.pos[0] > WIDTH - 30:
+        red_player.pos[0] = WIDTH - 30
+    elif red_player.pos[0] < 0:
+        red_player.pos[0] = 0
+    if blue_player.pos[0] > WIDTH - 30:
+        blue_player.pos[0] = WIDTH - 30
+    elif blue_player.pos[0] < 0:
+        blue_player.pos[0] = 0
+    
+def apply_physics():
+    check_rel_position()
+    fall()
+    wall_collision()
+
